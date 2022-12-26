ea7d83bb3a6a51d8e9e285a1aa6409f36dd12318
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-26 16:39:38 +0530

    Add files via upload

---
+++ day5program9.py
@@ -0,0 +1,13 @@
+##s0="*\t*\n**\t**\n***\t***\n"
+##s1="I said,\"This is a valid string.\""
+##print("s0:",s0)
+##print("s1:",s1)
+##print('\\'*4)
+##print('\\\n'*3)
+##print('Good-bye')
+print(len('George'))
+print(len(' Tom  '))
+s="""Hi
+sis!
+"""
+print(len(s))
