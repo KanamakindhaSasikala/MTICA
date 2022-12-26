eb637c08bbfc39b0fa193e3524c24696e86a31bd
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-26 16:38:30 +0530

    Add files via upload

---
+++ day5program3.py
@@ -0,0 +1,28 @@
+Python 3.11.0a5 (main, Feb  3 2022, 19:32:53) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: D:/pythonpractice_53/day5/day5program1.py
+256
+40
+
+= RESTART: D:/pythonpractice_53/day5/day5program1.py
+256
+-8
+
+= RESTART: D:/pythonpractice_53/day5/day5program2.py
+Hello World
+value of x is 8 value of y is 10
+s1
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    s1
+NameError: name 's1' is not defined
+s1="""This
+is a multi-line
+string"""
+s1
+'This\nis a multi-line\nstring'
+print(s1)
+This
+is a multi-line
+string
