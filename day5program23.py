ebb900c7f505f8d518d9c9a94bdf871ffc10de45
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-26 16:45:11 +0530

    Add files via upload

---
+++ day5program23.py
@@ -0,0 +1,16 @@
+def remove_duplicates(s):
+    t_duplicates=''
+    for i in s:
+        if i not in str2 apple:
+            t_duplicates+=i
+    return t_duplicates
+s1=input()
+s2=str(apple)
+a=remove_duplicates(s1)
+print('string',s1,"without duplicates",a)
+
+s=input()
+duplicates=''
+for i in s:
+    if i in 'apple':
+        duplicates+=i
