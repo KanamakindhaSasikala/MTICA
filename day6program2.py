bc78a8bbed2b021b70302c7c5385923cc1001650
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-27 16:32:22 +0530

    Add files via upload

---
+++ day6program2.py
@@ -0,0 +1,25 @@
+##lstEven=[]
+##lstOdd=[]
+##while(True):
+##   n=int(input("enter a value(-1 to end):"))
+##   if n==-1:
+##        break  
+##   
+##   elif(n%2==0):
+##           lstEven.append(n)
+##   elif(n%2!=0):                                                                                                                                                                                     one=2
+##           lstOdd.append(n)
+####lstEven.sort()
+####lstOdd.sort()
+######print(lst)
+##print("Even:",*lstEven)
+##print("min:",min(lstEven))
+##print("max:",max(lstEven))
+##print("avg:",round(sum(lstEven)/len(lstOdd),1))
+##      
+##print("Odd:",*lstOdd)
+##print("min:",min(lstOdd))
+##print("max:",max(lstOdd))
+##print("avg:",round(sum(lstOdd)/len(lstOdd),1))
+##
+one=2
