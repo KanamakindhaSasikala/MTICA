36a65f8a0993b53a69e87d14e9429939e779319d
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-27 16:35:04 +0530

    Add files via upload

---
+++ day6program9.py
@@ -0,0 +1,10 @@
+coord=(3,5)
+abc=(2,9)
+type(coord)
+<class 'tuple'>
+type(abc)
+<class 'tuple'>
+abc[0]
+2
+print('x: {0[0]}; y: {0[1]};abc : {1[0]},{1[1]}'.format(coord,abc))
+x: 3; y: 5;abc : 2,9
