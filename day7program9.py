e7c4f2db91983bcc989431fba13058f5e4026cf3
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-28 16:52:15 +0530

    Add files via upload

---
+++ day7program9.py
@@ -0,0 +1,8 @@
+#create a list from the elements of a range from 1200-2000with 130 
+##ans=[]
+##for i in range(1200,2001,130):
+##    ans.append(i)
+##print(ans)
+
+ans= [i for i in range(1200,2001,130)]
+print(ans)
