e83909162ec28a23c9bb1a49a9fcf0dd616b78a3
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-28 16:51:34 +0530

    Add files via upload

---
+++ day7program8.py
@@ -0,0 +1,10 @@
+##m=[[1,2],[3,4],[5,6],[7,8]]
+##ans=[[m[col][row] for col in range(len(m)) ]for row in range(len(m[0]))]
+##print(*ans)
+##
+
+lst1=[1,2,3,4,5,6]
+ans=[]
+for i in lst1:
+    ans.append(i)
+print(ans)
