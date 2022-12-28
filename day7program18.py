362c5cfb430526e19388aa41cd7f700a3c8b3d93
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-28 16:55:51 +0530

    Add files via upload

---
+++ day7program18.py
@@ -0,0 +1,25 @@
+'''nums=[11,22,33,44,55]
+import math
+print(nums)
+
+result=list(map(math.sqrt,nums))
+print(result)
+ 
+result=[math.sqrt(i) for i in nums]
+
+
+result=[]
+for i in nums:
+ '''
+def add_five(x):
+    temp=x+5
+    return temp
+
+nums=[11,22,33,44,55]
+result=list(map(add_five,nums))
+print(nums)
+print(result)
+print('_'*40)
+
+result=[i+5 for i in nums ]
+print(result)
