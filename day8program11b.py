b5b0dc4ccf96d2bebfd454b498cd32daafe5be3d
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-29 16:54:28 +0530

    Add files via upload

---
+++ day8program11b.py
@@ -0,0 +1,9 @@
+def addNumber(n1,n2):
+    res=n1+n2
+    return res
+for i in range(3):
+    n1=int(input("enter a number:"))
+    n2=int(input("enter a number:"))   
+    print(n1,'+',n2,'=',addNumber(n1,n2),sep=' ')
+
+ 
