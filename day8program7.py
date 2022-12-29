225074c2c2d47567850f9f32abe092ff0bac212d
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-29 16:49:47 +0530

    Add files via upload

---
+++ day8program7.py
@@ -0,0 +1,18 @@
+##num1=input("enter a number:")
+##num2=input("enter a number:")
+##
+##try:
+##    res=int(num1)/int(num2)   
+##except ZeroDivisionError:
+##    print("exception handled by Radha")
+##except  ValueError:
+##    print("exception handled by Ram")
+##except Exception as ob                                                                                                        :
+##    print(ob)
+##else:
+##    print(num1, '/' ,num2, '=', res)
+##finally:
+####    print("Thanks")
+##    
+##print("Visit again at python class at MTICA")
+##
