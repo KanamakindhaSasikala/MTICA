d9b356807989f53476f4400bee026cf44c772c4f
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-30 16:55:19 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,27 @@
+##def printSeriesIncreasing(ch,n):
+##    for i in range(1,n+1,1):
+##         print(ch*i)
+##    return None
+##def printSeriesDecreasing(ch,n):
+##    for i in range(n,0,-1):
+##        print(ch*i)
+##    return None
+##nch=input("enter a character:")
+##
+
+def printSeriesDecreasing(ch,n):
+    assert isinstance(ch,str),'first argument shoud be string'
+    assert isinstance(n,int),'second argument shoud be string'
+    for i in range(n,0,-1):
+        print(ch*i)
+    return None
+inpch=input("enter a character:")
+inpNum=int(input("enter a number:"))
+
+print('-',20)
+try:
+    printSeriesDecreasing(inpch,inpNum)
+except AssertionError as ob:
+     print(ob)
+     
+##printSeriesDecreasing(inpch,inpNum)
