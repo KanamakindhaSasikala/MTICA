5d06923ae0a212d148b3e16b9a93def7ca3bdc45
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-30 17:01:06 +0530

    Add files via upload

---
+++ day9program2.py
@@ -0,0 +1,8 @@
+##pattern program
+##def  printSeries(n):
+n=int(input("enter a number of rows:"))
+for i in range(1,n+1):
+         for j in range(1,n+1):
+              print(i,end=' ')
+              
+         print()
