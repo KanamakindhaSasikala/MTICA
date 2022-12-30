20fc155df3a27421b15c3c46a29a1a631ceea4f1
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-30 16:56:19 +0530

    Add files via upload

---
+++ day9program7a.py
@@ -0,0 +1,17 @@
+####logical operators
+####logical:AND
+##
+##print(True and True)
+##print(True and False)
+##
+####logical:OR
+##print(True or False)
+##
+####logical:NOT
+##
+##print(not True)
+
+a=5
+b=6
+##
+##print((a>2) and (b>=6))
