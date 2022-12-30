cc0e8e27a7a5f0ffd55ea7b01d8a891122de68d1
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-30 16:56:45 +0530

    Add files via upload

---
+++ day9program7.py
@@ -0,0 +1,25 @@
+##x1=5
+##y1=5
+##x2='Hello'
+##y2='Hello'
+##x3=[2,3,4]
+##y3=[4,5,6]
+##
+##print(x1 is y1)
+##print(x1 is not y1)
+##print(x2 is y2)
+##print(x3 is y3)
+message= 'Hello world'
+dict1={1:'a',2:'b'}
+
+print('H' in message)
+
+print('Hello' not in message)
+
+print(1 in dict1)
+
+print('a' in dict1)
+lst1=[10,20,30,'c','hello','java','python']
+print('java' in lst1)
+print('R'in lst1)
+print('R' not in lst1)
