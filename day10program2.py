e3fc5769f2de864ff6a96a6a56e4906f4627475a
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-31 16:41:27 +0530

    Add files via upload

---
+++ day10program2.py
@@ -0,0 +1,14 @@
+##def fun(str):
+def fun(str1):
+    print(str1)
+    return
+fun("good morning!")
+fun("sasikala------")
+
+def printme(str,n):
+    n[0]='prasad'
+    print(str,n)
+    return
+s=['ram','ravi']
+printme("wakeup",s)
+print('s:',s)
