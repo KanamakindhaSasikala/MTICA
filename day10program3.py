cc2743db853d898dbe19401917f33906fca82b45
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-31 16:41:44 +0530

    Add files via upload

---
+++ day10program3.py
@@ -0,0 +1,24 @@
+####def fun(str):
+##def fun(str1):
+##    print(str1)
+##    return
+##fun("good morning!")
+##fun("sasikala------")
+##
+##def printme(str,n):
+##    n[0]='prasad'
+##    print(str,n)
+##    return
+##s=['ram','ravi']
+##printme("wakeup",s)
+##print('s:',s)
+
+def changeMe(str2):
+    lst2=['kavya','latha','lalitha']
+    lst2[1]=['kavya','latha','lalitha']
+    
+    print(lst2)
+    return
+lst=[1,44,5,66,78]
+print(changeMe(lst))
+print('lst:',lst)
