d58e62b6f0ec0b9dfca30e709bb0b3b1f04067a9
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-31 16:44:07 +0530

    Add files via upload

---
+++ day10program7.py
@@ -0,0 +1,8 @@
+n=open (r"D:\pythonpractice_53\day10\file123.txt","w+",)
+for i in range(5):
+    inpstr=input("enter name:")
+    n.write(inpstr+'\n')
+n.close()
+print("written to file")
+        
+      
