911f0d302a7e125bb4fe53029805e654f20ce01c
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2022-12-31 16:42:19 +0530

    Add files via upload

---
+++ day10program4.py
@@ -0,0 +1,8 @@
+def printDeatail(name, marks):
+    print("name:",name)
+    print("marks:",marks)
+    return None
+##printDeatail()
+##printDeatail('sasi')
+##printDeatail('sasikala',89)
+printDeatail(name='sasi',marks=98)
