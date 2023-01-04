faa6892391b02269e22398cf4d9a0ebb4428d847
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2023-01-04 15:37:42 +0530

    Add files via upload

---
+++ day13program7.py
@@ -0,0 +1,21 @@
+def sum_series(a,b):
+    assert (a<b),"first argument should be smaller than second"
+    total=0
+    for i in range(a,b,1):
+        total=total+i
+        yield total
+
+
+n1=int(input())
+n2=int(input())
+obj=sum_series(n1,n2)
+x=0
+try:
+     while x<10:
+         print(next(obj))
+         x=x+1
+except AssertionError as x:
+     print(x)
+
+         
+    
