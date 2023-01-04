06d46dd24f6f7f7865e8a7c090d1202ddfb1347d
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2023-01-04 15:14:45 +0530

    Add files via upload

---
+++ day13program1.py
@@ -0,0 +1,13 @@
+a=input()
+##lst=apple gua mango:
+t=a.split(',')
+for i in len(t):
+  ans=[lenth(i) for i in t]
+  print(*ans)
+##def solveProblem(s):
+##    lst=s.split()
+##    return[lenth(i) for i in lst]
+##
+##
+##inp=input()
+##print(*solveProblem(inp))
