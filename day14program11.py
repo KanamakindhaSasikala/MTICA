06cc6396b75a759293a466831b4ff0bc82c59a96
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2023-01-05 16:24:28 +0530

    Add files via upload

---
+++ day14program11.py
@@ -0,0 +1,19 @@
+class Complex:
+    def __init__(self,real,image):
+        self.real=real
+        self.image=image
+    def __sub__(self,ob):
+        temp=self.real-ob.real,self.image-ob.image
+        return  temp
+    def __str__(self):
+        return(self.real,self.image)
+ob1=Complex(3,5)
+ob2=Complex(2,6)
+ob3=ob1-ob2
+print(ob3)
+
+'''
+output:
+==============
+(1, -1)
+'''
