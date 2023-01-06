a6079cd7f4753f91f651f3e53d31939158e346e2
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2023-01-06 16:52:29 +0530

    Add files via upload

---
+++ day15program2.py
@@ -0,0 +1,12 @@
+class Vehicle():
+    def __init__(self,max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+modelx= Vehicle(200,20)
+print(modelx.max_speed,modelx.mileage)
+
+'''
+output:
+===============
+200 20
+'''
