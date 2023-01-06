33b0cf8273214dc0487fd2b406612ed4af1288e7
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2023-01-06 16:53:56 +0530

    Add files via upload

---
+++ day15program6.py
@@ -0,0 +1,34 @@
+#lcm of a numbers:
+def  findLCM(n1,n2):
+    if n1<0 or n2<0:
+        return "INVALID"
+    if n1>n2:
+        n1,n2=n2,n1
+    i=n1
+    while True:
+         if i%n1==0 and i%n2==0:
+             return i
+         else:
+             i+=1
+    return None
+     
+print("Enter two numbers")
+a=int(input())
+b=int(input())
+print(findLCM(a,b))
+
+'''
+output:
+==============
+Enter two numbers
+20
+30
+60
+
+'''
+
+
+
+
+
+    
