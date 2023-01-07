55db18d6fc7fe1c5fd10001378c6af3bcb16d2cc
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2023-01-07 15:58:11 +0530

    Add files via upload

---
+++ day16program1.py
@@ -0,0 +1,15 @@
+lst=[1,2,3,4]
+it=iter(lst)
+for x in it:
+    print(x, end="  ")
+import sys
+while True:
+    try:
+        print (next(it))
+    except StopIteration as ob:
+        print(ob)
+        break
+'''
+output:
+1  2  3  4  
+'''
