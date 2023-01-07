678f84d47048e27332595cb76b21909d6d78b0c9
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2023-01-07 15:58:38 +0530

    Add files via upload

---
+++ day16program3.py
@@ -0,0 +1,15 @@
+def removeSpecialChar(s):
+    ans=[]
+    for i in s:
+        if(ord(i)in range(65,91)or ord(i) in range(97,123)or ord(i) in range(48,57)):
+            ans.append(i)
+    return ''.join(ans)
+inp=input()
+print(removeSpecialChar(inp))
+
+'''
+output:
+========
+rftdsae3g3g4840kqlehhsulkfdu76485
+
+'''
