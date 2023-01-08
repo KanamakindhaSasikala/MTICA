ddf21e10d7d64eb576dd29f48fbf685a487131e8
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2023-01-08 15:24:26 +0530

    Add files via upload

---
+++ day17program5.py
@@ -0,0 +1,15 @@
+from tkinter import*
+master=Tk()
+demo_text="this is a sample demo of message widget."
+msg=Message(master,text=demo_text)
+msg.config(bg='lightgreen',font=('times',24,'italic'))
+msg.pack()
+'''
+output:
+=========
+this is a sample
+demo of
+message widget.
+
+
+'''
