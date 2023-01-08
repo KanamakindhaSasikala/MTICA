c3f2485f79c18490f81c27dc9b7711b133319b3d
K.Sasikala Kanamakindha <121227182+KanamakindhaSasikala@users.noreply.github.com>
2023-01-08 15:24:09 +0530

    Add files via upload

---
+++ day17program4.py
@@ -0,0 +1,13 @@
+import datetime
+ob=datetime.datetime.now()
+
+str1=str(ob.day)+'_'+str(ob.month)+'_'+str(ob.year)
+str2=str(ob.hour)+'_'+str(ob.minute)+'_'+str(ob.second)
+str3='Backup_'+str1+'_'+str2+'.db'
+print(str3)
+
+'''
+output:
+=================
+Backup_8_1_2023_12_26_13.db
+'''
